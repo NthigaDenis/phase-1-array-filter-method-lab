551dceeeca7771820c6ff45be9179e56868b21a2
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-08-24 22:52:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string) {
+    let myDriver = [];
+    drivers.filter(function (driver) {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            myDriver.push(driver);
+        }
+    })
+    return myDriver;
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+}
+function matchName(drivers, string) {
+    let newDriver = [];
+    drivers.filter(function (driver) {
+        if (driver.name === string) {
+            newDriver.push(driver);
+
+        }
+    })
+    return newDriver;
+}
